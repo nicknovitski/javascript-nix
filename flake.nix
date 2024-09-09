a0b332f0fdb205164253af1568bf1cecee57499e
Nick Novitski <github@nicknovitski.com>
2024-09-08 19:18:47 -0700

    Add flake-compat and default.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 {
-  description = "";
+  inputs = {
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
+  };
 
   outputs = _: {
     shellModules.default = {imports = [./shell-modules];};
